1ed3adefb67edfa2e1509fa54847db4fb90d36a7
Сергей Сергиенко <sergo100@yandex.ru>
2025-09-24 03:48:01 +0500

    добавил новый тест

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,13 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    S = int(input())
-    petya = S // 6
-    serezha = petya
     
-    katya = 4 * petya
 
-    print(petya, katya, serezha)
+    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
--- tasks/task2.py
+++ tasks/task2.py
@@ -3,13 +3,9 @@
 def solve():
 # Ниже пишите решение задачи
 
-   x,y,z=map(int,input().split())
-   pencil=3
-   pen=pencil+2
-   flomaster=pen+7
-   res=x*pencil+y*pen+z*flomaster
-   print(res) 
+   
 
+   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
