e65ccbaadead9b03dce66d9e14df368c14aacb97
KirillSergeev666 <cyrills3rgeeff@yandex.ru>
2025-10-29 01:12:32 +0500

    Add files via upload

---
+++ работы/python-testv3-0-0-achous-main/tasks/task1.py
@@ -0,0 +1,13 @@
+# tasks/task1.py
+
+def solve():
+# Ниже пишите решение задачи
+    number=int(input())
+    hundreds = number // 100          
+    tens = (number // 10) % 10        
+    units = number % 10  
+    print(hundreds+tens+units)
+
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
